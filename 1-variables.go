1849ce2035f594c8aaf3430bdc26175d9b6c86c3
ms038889 <mohamed.sultan@broadcom.com>
2020-12-03 17:35:58 +0530

    1st commit

---
+++ 1-variables.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	a = 10
+	b = "abc"
+)
+var mk int =90
+func main() {
+	// Declare Variable
+	var v float32; // static 
+	
+	// Initialize
+	v = 10; // or (var v int = 10)
+
+	// Another Way
+	var i int = 10.0;
+
+	fmt.Println(i, v);
+
+	// format specifier
+	fmt.Printf("%v %T", b, a);
+	// %v -> value
+	// %T -> type
+}
